cf22214dbe22e5bf7267589fff73d7ec46c01157
taony <taony@users.noreply.github.com>
2018-06-02 10:37:12 +0800

    let

--- ES-6/01-let.js
+++ ES-6/01-let.js
@@ -1,16 +1,18 @@
 //1、ES6 新增了let命令，用来声明变量。它的用法类似于var，但是所声明的变量，只在let命令所在的代码块内有效。
+
 {
     let a = 6;
     console.log(a);
 }
 
-console.log(a);
+try {
+    console.log(a);
+} catch (ex) {
+    console.log(ex);
+}
 
 
-var b=7;
 
-console.log(a+b);
-
-
-
+let [e,f,g]=[1,2,3]
+console.log(e);
 
